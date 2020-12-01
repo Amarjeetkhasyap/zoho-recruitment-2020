d6628d69ecc24df77854e3c919c834e636ce668e
अmarjeet कashyap <31450890+Amarjeetkhasyap@users.noreply.github.com>
2020-12-01 10:26:35 +0530

    Create  zoho programming test.py

---
+++ zoho programming test.py
@@ -0,0 +1,18 @@
+n=5
+i=1
+a=1
+while i<=n:
+    j=1
+    while j<=n-i:
+        print(" ",end="  ")
+        j=j+1
+    f=1
+    while f<=i:
+        print(a,end="  ")
+        f=f+1
+        a=a+1
+    print()
+    i=i+1
+        
+        
+        
